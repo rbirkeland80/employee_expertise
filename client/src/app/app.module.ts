a43f83b0d7a5a7a5ccf8d32045372233e6b786e9
John Birkeland <johnbirkeland80@gmail.com>
2018-03-05 18:13:20 +0200

    add routing, core module, home and core components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
 
 
 @NgModule({
@@ -11,6 +13,8 @@ import { AppComponent } from './app.component';
     ],
     imports: [
         BrowserModule.withServerTransition({appId: 'ee-app'}),
+        AppRoutingModule,
+        CoreModule
     ],
     providers: [],
     bootstrap: [AppComponent]
---
+++ client/src/app/core/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Response } from '@angular/http';
+
+
+@Component({
+    selector: 'ee-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent { }
